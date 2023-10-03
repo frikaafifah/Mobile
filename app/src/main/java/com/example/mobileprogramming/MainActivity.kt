010146ab71c5c589cff774f3b5a19f93d9f82f80
frikaafifah <frikaafifah@gmail.com>
2023-10-03 08:35:16 +0700

    Intent

--- app/src/main/java/com/example/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobileprogramming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,36 +10,28 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
-    private lateinit var inputPanjang: EditText
-    private lateinit var inputLebar: EditText
-    private lateinit var inputTinggi: EditText
-    private lateinit var button: Button
-    private lateinit var result: TextView
+    private lateinit var btn_switch: Button
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_switch = findViewById(R.id.btn_switch)
+        btn_switch.setOnClickListener(this)
 
-        inputPanjang=findViewById(R.id.input_panjang)
-        inputLebar=findViewById(R.id.input_lebar)
-        inputTinggi=findViewById(R.id.input_tinggi)
-        button=findViewById(R.id.btn)
-        result=findViewById(R.id.result)
-
-        button.setOnClickListener(this)
 
     }
 
 
 
     override fun onClick(v: View?) {
-        if(v?.id == R.id.btn){
-            var panjang = inputPanjang.text.toString().trim()
-            var lebar = inputLebar.text.toString().trim()
-            var tinggi = inputTinggi.text.toString().trim()
-            val volume = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
-            result.text = volume.toString()
+        when (v?.id){
+            R.id.btn_switch -> {
+                val move = Intent(this@MainActivity, ActivityMove::class.java)
+                startActivity(move)
+            }
 
         }
     }
