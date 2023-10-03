e7c03cc6612b669f81e58c5aadec5c74302ab389
frikaafifah <frikaafifah@gmail.com>
2023-10-03 09:49:46 +0700

    Move With Data

--- app/src/main/java/com/example/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     private lateinit var btn_switch: Button
+    private lateinit var btn_data: Button
 
 
 
@@ -19,18 +18,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_switch = findViewById(R.id.btn_switch)
+        btn_data = findViewById(R.id.btn_move_data)
         btn_switch.setOnClickListener(this)
+        btn_data.setOnClickListener(this)
 
 
     }
 
-
-
     override fun onClick(v: View?) {
         when (v?.id){
             R.id.btn_switch -> {
-                val move = Intent(this@MainActivity, ActivityMove::class.java)
-                startActivity(move)
+                val moveIn = Intent(this@MainActivity, ActivityMove::class.java)
+                startActivity(moveIn)
+            }
+
+            R.id.btn_move_data -> {
+                val intentData = Intent(this@MainActivity, MoveWithData::class.java)
+                intentData.putExtra(MoveWithData.EXTRA_NAME, "frika afifah")
+                intentData.putExtra(MoveWithData.EXTRA_AGE, 20)
+
+                startActivity(intentData)
             }
 
         }
---
+++ app/src/main/java/com/example/mobileprogramming/MoveWithData.kt
@@ -0,0 +1,25 @@
+package com.example.mobileprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithData : AppCompatActivity() {
+    companion object{
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+        val receiveData = findViewById<TextView>(R.id.dataReceive)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+        val text = "Name: $name, Your age: $age"
+        receiveData.text = text
+
+
+
+    }
+}
